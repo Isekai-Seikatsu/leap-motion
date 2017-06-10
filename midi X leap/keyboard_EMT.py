3bc7808ca245fec087296ddb6f282d7ff23787a5
Isekai-Seikatsu <brian1689878638@gmail.com>
2017-06-11 06:52:59 +0800

    Add files via upload

--- midi X leap/keyboard_EMT.py
+++ midi X leap/keyboard_EMT.py
@@ -1,7 +1,6 @@
 
 from visual import *
 scene = display(title='keyboard',width=800,height=600,background=(0.5,0.6,0.5))
-
 def octave(L=1.,pitch=3):
     gap=L*0.0618
     length=(L+gap)*7
@@ -12,17 +11,24 @@ def octave(L=1.,pitch=3):
     for i in range(7):box(frame=unit, height= H, length=L, width= W, x=i*(L+gap)+x0)
     for i in 0,1,3,4,5:box(frame=unit, height= h, length=l, width= w, x=i*(L+gap)+x0+L/2, y=(H-h)/2, z=(w-W)/2, color=(0,0,0))
     j = 0
-    for i in sorted(unit.objects, key = lambda key:key.x):
+    unit.sorted_ob = sorted(unit.objects, key = lambda key:key.x)
+    for i in unit.sorted_ob:
         i.note = (pitch-3) + 60 + j
+        i.backup = (False, i.color)
+        #i.play
         j += 1
     return unit
 
-def keyboard(finger_width = 25, pitch_range = (1, 6)):
+def keyboard(finger_width = 25., pitch_range = (1, 6)):
     keyboard = frame()
+    keyboard.keys = []
     for pitch in range(*pitch_range):
-        octave(finger_width, pitch).frame = keyboard
+        unit = octave(finger_width, pitch)
+        unit.frame = keyboard
+        keyboard.keys = keyboard.keys + unit.sorted_ob
+
     keyboard.up = (0, 0, -1)
-    return keyboard, 1.0618*finger_width*7
+    return keyboard, (1.0618*finger_width*7, 6.472*finger_width, finger_width)
 
 if __name__=='__main__':
     keyboard()
--- midi X leap/tryhard0.py
+++ midi X leap/tryhard0.py
@@ -11,18 +11,21 @@ keyboard, octave_LHW = keyboard()
 sthop = 100#set the height of piano
 keyboard.y += sthop
 scene.center.y += sthop
-
+red_ball=[sphere(color=(1,0,0), radius = 10) for _ in range(10)]
 while True:
             rate(100)
             frame = controller.frame()
             i=0
             keys_is_pressed = [False] * 60
+            j=0
             for hand in frame.hands:
                 for finger in hand.pointables:
+                    red_ball[j].pos = finger.tip_position.to_tuple()
                     if  all([ 1  if l < p < u else 0 for l, p, u in zip((-2*octave_LHW[0], -octave_LHW[2]*0.927+sthop, -octave_LHW[1]/2.), finger.tip_position, (3*octave_LHW[0], octave_LHW[2]*0.691+sthop, octave_LHW[1]/2.))]):
                         x = finger.tip_position.x#hand.palm_position.x
                         _, key = min(((abs(key.pos.x - x), key) for key in keyboard.objects[int((x+octave_LHW[0]*2)/octave_LHW[0])].objects))
                         keys_is_pressed[key.note-36] = True
+                    j+=1
 
 
                 hands[i].update(hand)
